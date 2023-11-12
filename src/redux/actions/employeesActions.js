c2e5d0e14560b17e9a3d88bebe22261da8c1a302
Lucas <lucasbombaron@gmail.com>
2023-11-12 20:23:52 +0100

    build

--- src/redux/actions/employeesActions.js
+++ src/redux/actions/employeesActions.js
@@ -1,3 +1,9 @@
+/**
+ * Action pour ajouter un nouvel employé au store Redux.
+ *
+ * @param {Object} employee - L'objet représentant un employé avec toutes ses données.
+ * @returns {Object} Une action Redux avec le type 'ADD_EMPLOYEE' et l'employé à ajouter comme payload.
+ */
 export const addEmployee = (employee) => {
     return {
         type: 'ADD_EMPLOYEE',
--- src/redux/reducers/employeesReducer.js
+++ src/redux/reducers/employeesReducer.js
@@ -1,16 +1,26 @@
+// Définit l'état initial pour le reducer des employés.
 const initialState = {
-    employees: [],
+    employees: [], // Initialisation du tableau des employés.
 };
 
+/**
+ * Reducer pour gérer les actions liées aux employés.
+ *
+ * @param {Object} state - L'état actuel de la partie des employés dans le store Redux.
+ * @param {Object} action - L'action dispatchée.
+ * @returns {Object} - Le nouvel état après application de l'action.
+ */
 function employeesReducer(state = initialState, action) {
-    console.log('Reducer action:', action);
+    // Gère les actions en fonction de leur type.
     switch (action.type) {
         case 'ADD_EMPLOYEE':
+            // Ajoute un nouvel employé à la liste des employés.
             return {
-                ...state,
-                employees: [...state.employees, action.payload],
+                ...state, // Copie l'état actuel.
+                employees: [...state.employees, action.payload], // Ajoute le nouvel employé à la liste.
             };
         default:
+            // Retourne l'état actuel si aucune action correspondante n'est trouvée.
             return state;
     }
 }

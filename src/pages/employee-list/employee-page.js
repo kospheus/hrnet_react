4a0c9cd5ea3cb2723acaab52f8572ac2f4da8820
Lucas <lucasbombaron@gmail.com>
2023-11-13 12:41:45 +0100

    - LazyList

--- src/pages/employee-list/employee-page.js
+++ src/pages/employee-list/employee-page.js
@@ -1,9 +1,6 @@
-import { lazy, Suspense } from 'react';
 import React from "react";
 import HeaderCurrentEmployee from '../../components/header/headerCurrentEmployee';
-
-const LazyList = lazy(() => import('../../components/list/list'));
-
+import List from "../../components/list/list";
 
 function EmployeeList() {
 
@@ -11,9 +8,7 @@ function EmployeeList() {
         <div className="App">
             <HeaderCurrentEmployee />
             <h1>Current Employees</h1>
-            <Suspense fallback={<div>Loading...</div>}>
-                <LazyList />
-            </Suspense>
+            <List />
         </div>
     )
 };
